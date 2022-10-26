9b197598ae94f883f2d8d9e6cdcacaa68bf063fa
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-10-25 22:30:38 -0300

    Swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import swaggerUI from 'swagger-ui-express'
 
+import swaggerFile from './swagger.json'
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
@@ -7,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 export { app };
